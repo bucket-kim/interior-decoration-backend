4caf30a72af3ffed2bc71ba76c2ec8c27e790828
Brian Kim <brian.kyounghoon.kim@gmail.com>
2025-01-21 11:45:36 -0500

    create user repository with prisma

--- prisma/migrations/20250117202729_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `Rrole` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "Rrole",
-ADD COLUMN     "roles" "Role"[] DEFAULT ARRAY['USER']::"Role"[];
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,27 @@
-import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
-import cookieParser from 'cookie-parser';
-import { connectToDB } from './config/database';
-import { HttpException } from './utils/HttpExceptions';
-import ErrorHandler from './middlewares/ErrorHandler';
-import { AppRoutes } from './routes/AppRoutes';
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+import { connectToDB } from "./config/db";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-
-app.use((req: Request, res: Response, _next: NextFunction) => {
-    res.status(404).send("Request Not Found")
-})
-
-app.use("/api", AppRoutes);
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    next(new HttpException(404, "Route not found"));
+    res.status(400).send("Rout not found")
 })
 
-app.use(ErrorHandler);
-
-const initializeApp = async () => {
+const initializeApp = async() => {
     try {
-        app.listen(3001, () => {
-            console.log(`[server]: server is running at 
-                http://localhost:3000/api`);
-        })
         await connectToDB();
-    } catch(error) {
-        console.log(error);
+        app.listen(8080, () => {
+            console.log(`[server]: Server running at http://localhost/8080/api`)
+        })
+    }catch(error) {
+        console.error(error);
         process.exit(1)
     }
 }
 
-initializeApp()
+initializeApp();
